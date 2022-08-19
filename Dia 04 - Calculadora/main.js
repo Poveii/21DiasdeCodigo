9c8d9f780033de49564b501a1ab6d9f28e1f9c33
Poveii <yellowtutors@gmail.com>
2022-08-19 18:11:25 -0300

    :rocket: Day Four Completed!

---
+++ Dia 04 - Calculadora/main.js
@@ -0,0 +1,60 @@
+const displayElement = document.querySelector(".display");
+const numberButtonsElements = document.querySelectorAll(".number");
+const operatorsElements = document.querySelectorAll(".basic-operators button");
+const equalElement = document.querySelector("#equal");
+const clearDisplayElement = document.querySelector("#clear");
+const backspaceDisplayElement = document.querySelector("#backspace");
+
+operatorsElements.forEach((operatorElement) => {
+  let operator = operatorElement.textContent;
+
+  operatorElement.addEventListener("click", function () {
+    if (!displayElement.classList.contains("number")) return;
+    if (displayElement.classList.contains("equal"))
+      displayElement.innerHTML = eval(displayElement.textContent);
+
+    operatorElement.id === "multiplication" ? (operator = "*") : operator;
+    operatorElement.id === "division" ? (operator = "/") : operator;
+
+    displayElement.innerHTML += "<span>" + operator + "</span>";
+    document.querySelector(".display span").id = "operator";
+
+    displayElement.classList.remove("number");
+  });
+});
+
+numberButtonsElements.forEach((numberElement) => {
+  numberElement.addEventListener("click", function () {
+    let number = numberElement.textContent;
+
+    displayElement.innerHTML += number;
+    displayElement.classList.add("number");
+
+    let operatorIdElement = document.querySelector("#operator");
+    if (operatorIdElement) {
+      displayElement.classList.add("equal");
+    } else return;
+  });
+});
+
+equalElement.addEventListener("click", function () {
+  if (displayElement.classList.contains(!"equal")) return;
+
+  try {
+    eval(displayElement.textContent);
+  } catch (err) {
+    return (displayElement.innerHTML = displayElement.textContent.slice(0, -1));
+  }
+
+  displayElement.innerHTML = eval(displayElement.textContent);
+});
+
+clearDisplayElement.addEventListener(
+  "click",
+  () => (displayElement.innerHTML = "")
+);
+
+backspaceDisplayElement.addEventListener(
+  "click",
+  () => (displayElement.textContent = displayElement.textContent.slice(0, -1))
+);
