17557edfdd9aa66d5b5406a3752426004873beb9
Poveii <yellowtutors@gmail.com>
2022-08-29 15:58:30 -0300

    :rocket: Day Thirteen Finished!

--- Dia 13 - Menu Responsivo/main.js
+++ Dia 13 - Menu Responsivo/main.js
@@ -1,5 +1,6 @@
 const menuButtonElement = document.querySelector("nav span");
 const navListElement = document.querySelector("nav ul");
+const itemsNavList = document.querySelectorAll("ul li a");
 
 menuButtonElement.addEventListener("click", function () {
   if (menuButtonElement.textContent === " menu ") {
@@ -10,4 +11,13 @@ menuButtonElement.addEventListener("click", function () {
     menuButtonElement.textContent = " menu ";
     navListElement.classList.remove("show");
   }
+});
+
+itemsNavList.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    const activeItem = document.querySelector("a.active");
+    activeItem.classList.remove("active");
+
+    item.classList.add("active");
+  });
 });
