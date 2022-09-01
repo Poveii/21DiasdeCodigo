30b1845de39895a0e5d875585d9a90d30bb5456d
Poveii <yellowtutors@gmail.com>
2022-09-01 18:14:03 -0300

    :rocket: Day Fifteen Finished!

--- Dia 15 - E-Commerce/main.js
+++ Dia 15 - E-Commerce/main.js
@@ -1,13 +1,16 @@
-const menuButtonElement = document.querySelector("nav span");
+const menuButtonElement = document.querySelector("#menu-icon");
 const navListElement = document.querySelector("nav ul");
+const cartButtonElement = document.querySelector("#shopping-cart");
 
 menuButtonElement.addEventListener("click", function () {
   if (menuButtonElement.textContent === " menu ") {
     menuButtonElement.textContent = " close ";
 
     navListElement.classList.add("show");
+    cartButtonElement.classList.add("none");
   } else {
     menuButtonElement.textContent = " menu ";
     navListElement.classList.remove("show");
+    cartButtonElement.classList.remove("none");
   }
 });
