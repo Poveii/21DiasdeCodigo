0c9f2cb93297e2f932a7afee1598887846cfde67
Poveii <yellowtutors@gmail.com>
2022-09-02 18:11:16 -0300

    :rocket: Day Sixteen Finished!

--- Dia 16 - Anime API/main.js
+++ Dia 16 - Anime API/main.js
@@ -2,10 +2,10 @@ function randomNumberGenerator(item) {
   return Math.floor(Math.random() * item.length);
 }
 
-function setAnime() {
-  const url = "https://api.jikan.moe/v4/recommendations/anime";
+function setAnimeInformations() {
+  const recomendation_URL = "https://api.jikan.moe/v4/recommendations/anime";
 
-  fetch(url)
+  fetch(recomendation_URL)
     .then((response) => response.json())
     .then((api) => {
       const randomNumberToData = randomNumberGenerator(api.data);
@@ -13,33 +13,30 @@ function setAnime() {
         api.data[randomNumberToData].entry
       );
 
-      const animeId =
-        api.data[randomNumberToData].entry[randomNumberToEntry].mal_id;
-
-      const url2 = `https://api.jikan.moe/v4/anime/${animeId}/full`;
-
-      fetch(url2)
-        .then((response) => response.json())
-        .then((api2) => {
-          // console.log(api2.data);
-          animeSynopsis.textContent = api2.data.synopsis;
-        });
-
-      // console.log(api.data[randomNumberToData]);
-
       animeImage.src =
         api.data[randomNumberToData].entry[
           randomNumberToEntry
         ].images.webp.large_image_url;
 
+      animeRecomendationText.textContent = api.data[randomNumberToData].content;
+
       animeName.textContent =
         api.data[randomNumberToData].entry[randomNumberToEntry].title;
 
-      animeURL.href =
-        api.data[randomNumberToData].entry[randomNumberToEntry].url;
+      const animeId =
+        api.data[randomNumberToData].entry[randomNumberToEntry].mal_id;
 
-      animeRecomendationText.textContent = api.data[randomNumberToData].content;
+      const anime_URL = `https://api.jikan.moe/v4/anime/${animeId}/full`;
+
+      fetch(anime_URL)
+        .then((response) => response.json())
+        .then((api) => {
+          animeSynopsis.textContent = api.data.synopsis;
+        });
+
+      animeLink.href =
+        api.data[randomNumberToData].entry[randomNumberToEntry].url;
     });
 }
 
-setAnime();
+setAnimeInformations();
