fa5ba8aee640c2e35c2e9f37a953bf70c6fc8e1c
Poveii <yellowtutors@gmail.com>
2022-09-06 17:59:34 -0300

    :rocket: Day Twenty Finished!

--- Dia 20 - Emoji Picker/main.js
+++ Dia 20 - Emoji Picker/main.js
@@ -1,5 +1,8 @@
 const emojiRandomElement = document.querySelector("h1 span");
 const mainElement = document.querySelector("main");
+const favicon = document.querySelector("head link[rel~='shortcut']");
+const moreEmojisButton = document.querySelector("#more-emojis");
+const windowTextElement = document.querySelector(".window-text");
 
 const url =
   "https://emoji-api.com/emojis?access_key=670d4eacad2424782d4e81c433a007764e6e63b4";
@@ -8,6 +11,12 @@ fetch(url)
   .then((response) => response.json())
   .then((api) => {
     const randomNumber = Math.floor(Math.random() * api.length);
+    favicon.href = `data:image/svg+xml,
+      <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+        <text y=%22.9em%22 font-size=%2290%22>
+          ${api[randomNumber].character}
+        </text>
+      </svg>`;
     emojiRandomElement.textContent = api[randomNumber].character;
 
     let index = 0;
@@ -23,9 +32,47 @@ fetch(url)
 
     emojiCardsElements.forEach((cardElement) => {
       cardElement.addEventListener("click", function () {
-        let emojiCharacter = cardElement.querySelector("span").textContent;
+        const emojiCharacter = cardElement.querySelector("span").textContent;
 
         navigator.clipboard.writeText(emojiCharacter);
+
+        windowTextElement.classList.remove("transparent");
+        windowTextElement.innerHTML = `O emoji 
+          ${emojiCharacter} foi copiado para sua área de transferência!`;
+
+        setTimeout(() => windowTextElement.classList.add("transparent"), 4000);
       });
+    });
+
+    index = 56;
+    moreEmojisButton.addEventListener("click", function () {
+      const emojiCardsElements = document.querySelectorAll(".emoji-card");
+      emojiCardsElements.forEach((card) => {
+        let emoji = api[index];
+
+        console.log(emoji);
+        if (!emoji) {
+          windowTextElement.classList.remove("transparent");
+          windowTextElement.innerHTML = "Acabou os emojis por aqui!";
+
+          setTimeout(
+            () => windowTextElement.classList.add("transparent"),
+            4000
+          );
+
+          return;
+        }
+
+        card.querySelector("span").textContent = emoji.character;
+        card.querySelector("h2").textContent = emoji.unicodeName;
+
+        index++;
+      });
+
+      index += 56;
+
+      if (!api[index]) return;
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
     });
   });
