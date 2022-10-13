f19d05b74c0b6e700e7806691a061225d5fec50e
Poveii <yellowtutors@gmail.com>
2022-10-13 10:59:41 -0300

    :bug: Day Two - fix: Hello World to just one click

--- Dia 02 - Hello World/main.js
+++ Dia 02 - Hello World/main.js
@@ -1,7 +1,12 @@
 const helloWorldElement = document.querySelector("h1");
 const spanHelloWorldElement = document.querySelector("h1 span");
 
+let clickCounter = 0;
+
 helloWorldElement.addEventListener("click", function () {
+  if (clickCounter >= 1) return;
+  clickCounter++;
+
   helloWorldElement.classList.add("vanish");
   document.querySelector(".content").classList.add("vanish");
 
