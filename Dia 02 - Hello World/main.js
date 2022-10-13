891e202813b407be0cb62161f3c74bf9114217f7
Poveii <yellowtutors@gmail.com>
2022-10-13 10:20:22 -0300

    :recycle: Day Two - refactor: Removed some lines

--- Dia 02 - Hello World/main.js
+++ Dia 02 - Hello World/main.js
@@ -1,20 +1,16 @@
 const helloWorldElement = document.querySelector("h1");
 const spanHelloWorldElement = document.querySelector("h1 span");
-const contentElement = document.querySelector(".content");
-const mainElement = document.querySelector("main");
-const blurElement = document.querySelector(".blur");
-const footerElement = document.querySelector("footer small");
 
 helloWorldElement.addEventListener("click", function () {
   helloWorldElement.classList.add("vanish");
-  contentElement.classList.add("vanish");
+  document.querySelector(".content").classList.add("vanish");
 
   setTimeout(() => {
     helloWorldElement.classList.remove("vanish");
 
     setTimeout(() => {
-      mainElement.classList.add("unsplash");
-      blurElement.classList.add("show");
+      document.querySelector("main").classList.add("unsplash");
+      document.querySelector(".blur").classList.add("show");
       helloWorldElement.classList.add("colors");
 
       setTimeout(() => {
@@ -22,7 +18,7 @@ helloWorldElement.addEventListener("click", function () {
         spanHelloWorldElement.classList.add("type");
 
         setTimeout(() => {
-          footerElement.classList.add("show");
+          document.querySelector("footer small").classList.add("show");
         }, 3000);
       }, 4000);
     }, 800);
